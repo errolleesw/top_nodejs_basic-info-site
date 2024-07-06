41a35c3e5a7154e59190a1e8a91631a3d9d9d3b9
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-06 18:50:42 +0800

    Refactor to use express

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require('path');
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    const fileName = (pathName) => {
-        if (pathName === "/") {
-            return "index.html";
-        } else {
-            return "." + pathName + ".html";
-        }
-    }
-    var filename = fileName(q.pathname);
-    
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            fs.readFile('./404.html', function (error, errorData) {
-                if (error) {
-                    res.writeHead(500, { 'Content-Type': 'text/html' });
-                    return res.end("500 Internal Server Error");
-                }
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write(errorData);
-                return res.end();
-            });
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        }
-    });
-}).listen(8080);
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log('Server running at http://localhost:8080/');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','contact-me.html'));
+});
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`)
+});
